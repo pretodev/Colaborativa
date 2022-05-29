573a5931acb358460f8d17b2126a1c21919fac14
Silas <santorsilas@gmail.com>
2022-05-29 10:42:13 -0300

    refactor(app): criando arquivo para centralizar providers

---
+++ app/lib/app_providers.dart
@@ -0,0 +1,80 @@
+import 'package:colaborativa_app/core/clients/colaborativa_api_client.dart';
+import 'package:colaborativa_app/utils/context_extension.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:provider/provider.dart';
+
+import 'core/achievement_service.dart';
+import 'core/activities_service.dart';
+import 'core/app_service.dart';
+import 'core/auth_service.dart';
+import 'core/chat_service.dart';
+import 'core/feeling_service.dart';
+import 'core/score_service.dart';
+import 'core/user_service.dart';
+import 'ui/controllers/app_controller.dart';
+import 'ui/controllers/auth_controller.dart';
+
+final appProviders = [
+  Provider(
+    create: (ctx) => FirebaseAuth.instance,
+    lazy: true,
+  ),
+  Provider(
+    create: (ctx) => FirebaseDatabase.instance,
+    lazy: true,
+  ),
+  Provider(
+    create: (ctx) => ColaborativaApiClient(ctx.read()),
+    lazy: true,
+  ),
+  Provider(
+    create: (ctx) => AppService(),
+    lazy: true,
+  ),
+  Provider(
+    create: (ctx) => FeelingService(
+      ctx.userId,
+      db: ctx.read(),
+      colaborativaApi: ctx.read<ColaborativaApiClient>().client,
+    ),
+    lazy: true,
+  ),
+  Provider(
+    create: (ctx) => ActivitiesServices(
+      ctx.userId,
+      database: ctx.read(),
+    ),
+    lazy: true,
+  ),
+  Provider(
+    create: (ctx) => ChatService(),
+    lazy: true,
+  ),
+  Provider(
+    create: (ctx) => AuthService(),
+    lazy: true,
+  ),
+  Provider(
+    create: (ctx) => ScoreService(),
+    lazy: true,
+  ),
+  Provider(
+    create: (ctx) => AchievementService(),
+    lazy: true,
+  ),
+  Provider(
+    create: (ctx) => UserService(),
+    lazy: true,
+  ),
+  ChangeNotifierProvider<AppController>(
+    create: (ctx) => AppController(
+      appService: ctx.read<AppService>(),
+    ),
+  ),
+  ChangeNotifierProvider(
+    create: (ctx) => AuthController(
+      authService: ctx.read<AuthService>(),
+    )..loadUser(),
+  ),
+];
--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,23 +1,10 @@
-import 'package:colaborativa_app/core/clients/colaborativa_api_client.dart';
-import 'package:colaborativa_app/utils/context_extension.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:colaborativa_app/app_providers.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'core/achievement_service.dart';
-import 'core/activities_service.dart';
-import 'core/app_service.dart';
-import 'core/auth_service.dart';
-import 'core/chat_service.dart';
-import 'core/feeling_service.dart';
-import 'core/score_service.dart';
-import 'core/user_service.dart';
 import 'firebase_options.dart';
 import 'ui/app_widget.dart';
-import 'ui/controllers/app_controller.dart';
-import 'ui/controllers/auth_controller.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -28,69 +15,7 @@ void main() async {
 
   runApp(
     MultiProvider(
-      providers: [
-        Provider(
-          create: (ctx) => FirebaseAuth.instance,
-          lazy: true,
-        ),
-        Provider(
-          create: (ctx) => FirebaseDatabase.instance,
-          lazy: true,
-        ),
-        Provider(
-          create: (ctx) => ColaborativaApiClient(ctx.read()),
-          lazy: true,
-        ),
-        Provider(
-          create: (ctx) => AppService(),
-          lazy: true,
-        ),
-        Provider(
-          create: (ctx) => FeelingService(
-            ctx.userId,
-            db: ctx.read(),
-            colaborativaApi: ctx.read<ColaborativaApiClient>().client,
-          ),
-          lazy: true,
-        ),
-        Provider(
-          create: (ctx) => ActivitiesServices(
-            ctx.userId,
-            database: ctx.read(),
-          ),
-          lazy: true,
-        ),
-        Provider(
-          create: (ctx) => ChatService(),
-          lazy: true,
-        ),
-        Provider(
-          create: (ctx) => AuthService(),
-          lazy: true,
-        ),
-        Provider(
-          create: (ctx) => ScoreService(),
-          lazy: true,
-        ),
-        Provider(
-          create: (ctx) => AchievementService(),
-          lazy: true,
-        ),
-        Provider(
-          create: (ctx) => UserService(),
-          lazy: true,
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => AppController(
-            appService: ctx.read<AppService>(),
-          ),
-        ),
-        ChangeNotifierProvider(
-          create: (ctx) => AuthController(
-            authService: ctx.read<AuthService>(),
-          )..loadUser(),
-        ),
-      ],
+      providers: appProviders,
       child: const AppWidget(),
     ),
   );
