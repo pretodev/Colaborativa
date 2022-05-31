7ca2ff77b8b272ccc9ee46273184e62e6a0f13fe
Silas <santorsilas@gmail.com>
2022-05-31 17:09:40 -0300

    corrigindo visualização de mensagens

--- app/lib/ui/views/chat/widgets/chat_message_widget.dart
+++ app/lib/ui/views/chat/widgets/chat_message_widget.dart
@@ -26,7 +26,7 @@ class ChatMessageWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-
+    final bg = isMe ? AppColors.backgroundMessage : AppColors.backgroundDark;
     final chatItems = [
       Visibility(
         visible: isAvatarVisible,
@@ -34,46 +34,48 @@ class ChatMessageWidget extends StatelessWidget {
         child: const ChatAvatar(),
       ),
       const SizedBox(width: 4.0),
-      Expanded(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 12.0),
-          decoration: BoxDecoration(
-            color: AppColors.backgroundMessage,
-            borderRadius: BorderRadius.circular(20),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Visibility(
-                visible: isNameVisible,
-                child: Padding(
-                  padding: const EdgeInsets.only(bottom: 8.0),
-                  child: Text(
-                    message.emitter.name.capitalize,
-                    style: theme.textTheme.bodyText2?.copyWith(
-                      color: titleColor,
-                    ),
+      Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 12.0),
+        decoration: BoxDecoration(
+          color: bg,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Visibility(
+              visible: !isMe && isNameVisible,
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  message.emitter.name.capitalize,
+                  style: theme.textTheme.bodyText2?.copyWith(
+                    color: titleColor,
                   ),
                 ),
               ),
-              Text(
+            ),
+            ConstrainedBox(
+              constraints: const BoxConstraints(maxWidth: 250.0),
+              child: Text(
                 message.content,
                 style: theme.textTheme.bodyText2?.copyWith(color: Colors.black),
               ),
-              const SizedBox(height: 4.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    Jiffy(message.timestamp).fromNow(),
-                    style: theme.textTheme.bodyText2?.copyWith(fontSize: 12.0),
-                  ),
-                ],
-              ),
-            ],
-          ),
+            ),
+            const SizedBox(height: 4.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Text(
+                  Jiffy(message.timestamp).fromNow(),
+                  style: theme.textTheme.bodyText2?.copyWith(fontSize: 12.0),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
+      const Spacer(),
     ];
 
     return Padding(
